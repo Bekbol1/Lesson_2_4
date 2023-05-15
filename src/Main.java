1ca144e7055869dc86aa3703e4a24d4b6e8265ee
Bekbol1 <kbekbol9@gmail.com>
2023-05-15 19:56:39 +0600

    4 урок

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Black");
+        colors.add("White");
+        colors.add("Pink");
+        colors.add("Yellow");
+        System.out.println(colors);
+        System.out.println("-----------------------------------");
+        Collections.sort(colors);
+        System.out.println(colors);
+        System.out.println("---------------------");
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println(colors.get(i));;
+        }
+
+        for (String  b : colors) {
+            System.out.println(b);
+        }
+
+        System.out.println("--------------------------------------------");
+
+        Player player = new Player("Griezmann", 8);
+        Player player2 = new Player("Araujo", 4);
+        Player player3 = new Player("De Jong",21);
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(player);
+        players.add(player2);
+        players.add(player3);
+
+        System.out.println(players);
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Player {
+public class Player {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Player(String name, Integer number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    private String name;
+    private Integer number;
+
+    @Override
+    public String toString() {
+        return "\nPlayer " +
+                "\nИмя = " + name  +
+                "\nНомер игрока = " + number;
+    }
 }
